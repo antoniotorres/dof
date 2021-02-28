fb96110c6b18709507cf05037358392f9348a5b0
Antonio Torres <toniotgz@gmail.com>
2021-02-27 23:44:27 -0600

    feat(header): Replace img with Image

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Header() {
   return (
@@ -7,11 +8,14 @@ export default function Header() {
         <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
           <div>
             <span className="sr-only">Diario Oficial de la Fedaración</span>
-            <img
-              className="h-8 w-auto sm:h-10"
-              src="/sello.png"
-              alt="Sello de Mexico"
-            />
+            <div className="h-10 w-10">
+              <Image
+                src="/sello.png"
+                alt="Sello de Mexico"
+                width={50}
+                height={50}
+              />
+            </div>
           </div>
           <div>Diaro Oficial de la Fedaración</div>
         </div>
