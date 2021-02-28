783ca5a7a539a2a3a057420da1c0e3f2d1e57ff6
Antonio Torres <toniotgz@gmail.com>
2021-02-28 02:35:21 -0600

    feat(lib): Add metadata to files

--- lib/getNote.ts
+++ lib/getNote.ts
@@ -1,3 +1,4 @@
+import { formatISO, parse } from "date-fns";
 import { uploadFile } from "./aws";
 import { sanitizeHTML } from "./sanitize";
 
@@ -11,7 +12,7 @@ async function fetchFromSource(id: string) {
 
 async function fetchFromS3(id: string) {
   const response = await fetch(
-    `https://${process.env.SERVER_AWS_BUCKET}.s3.${process.env.SERVER_AWS_REGION}.amazonaws.com/${id}.php`,
+    `https://${process.env.SERVER_AWS_BUCKET}.s3.${process.env.SERVER_AWS_REGION}.amazonaws.com/${id}.json`,
   );
   const data = await response.text();
   if (response.status === 200) {
@@ -20,15 +21,37 @@ async function fetchFromS3(id: string) {
   throw new Error(data);
 }
 
+function findMetadata(data: string) {
+  const title = unescape(
+    data
+      .split("<div id='DivDetalleNota' align='justify'")[1]
+      .match(/<title>(.*)<\/title>/g)[0]
+      .replace("<title>", "")
+      .replace("</title>", ""),
+  );
+  const published_at = formatISO(
+    parse(
+      data
+        .match(/<b>DOF:(.*)<\/b>/g)[0]
+        .replace("<b>DOF:", "")
+        .replace("</b>", "")
+        .trim(),
+      "dd/MM/yyyy",
+      new Date(),
+    ),
+  );
+  return { title, published_at };
+}
+
 // Helper to make GET requests to Strapi
 export async function getNote(id: string) {
-  const filename = `${id}.php`;
+  const filename = `${id}.json`;
 
   try {
     const s3File = await fetchFromS3(id);
     console.log("File Found in S3 ... OK");
     if (s3File) {
-      return s3File;
+      return JSON.parse(s3File);
     }
   } catch (e) {
     console.error(e);
@@ -37,6 +60,9 @@ export async function getNote(id: string) {
   // Download file from original DOF page
   console.log("Not Found in S3 ... Fallback to DOF");
   const source = await fetchFromSource(id);
-  uploadFile(filename, sanitizeHTML(source));
-  return sanitizeHTML(source);
+  const metadata = findMetadata(source);
+  const content = sanitizeHTML(source);
+  const file = { metadata, content };
+  uploadFile(filename, JSON.stringify(file));
+  return file;
 }
