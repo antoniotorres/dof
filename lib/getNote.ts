ae8a8ba57ec1e3389a118ca39ffecc4a1099a7b3
Antonio Torres <toniotgz@gmail.com>
2021-02-28 00:58:35 -0600

    fix(lib): Change env var name

--- lib/getNote.ts
+++ lib/getNote.ts
@@ -11,7 +11,7 @@ async function fetchFromSource(id: string) {
 
 async function fetchFromS3(id: string) {
   const response = await fetch(
-    `https://${process.env.SERVER_BUCKET}.s3.${process.env.SERVER_AWS_REGION}.amazonaws.com/${id}.php`,
+    `https://${process.env.SERVER_AWS_BUCKET}.s3.${process.env.SERVER_AWS_REGION}.amazonaws.com/${id}.php`,
   );
   const data = await response.text();
   if (response.status === 200) {
@@ -26,12 +26,16 @@ export async function getNote(id: string) {
 
   try {
     const s3File = await fetchFromS3(id);
+    console.log("File Found in S3 ... OK");
     if (s3File) {
       return s3File;
     }
-  } catch (e) {}
+  } catch (e) {
+    console.error(e);
+  }
 
   // Download file from original DOF page
+  console.log("Not Found in S3 ... Fallback to DOF");
   const source = await fetchFromSource(id);
   uploadFile(filename, sanitizeHTML(source));
   return sanitizeHTML(source);
