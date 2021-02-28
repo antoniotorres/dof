80a2f0506dd2e41afc898676ad4dd1a46a9da598
Antonio Torres <toniotgz@gmail.com>
2021-02-27 22:52:06 -0600

    fix(header): Change typo in text

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="relative z-20 shadow">
         <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
           <div>
-            <span className="sr-only">Diaro Oficial de la Fedaración</span>
+            <span className="sr-only">Diario Oficial de la Fedaración</span>
             <img
               className="h-8 w-auto sm:h-10"
               src="/sello.png"
